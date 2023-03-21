f7d6fa594f1f0d291720ad1d5648ea7283b02f8e
Anukr98 <anubhav.kumar98@gmail.com>
2023-03-21 23:40:20 +0530

    finalised ejs as templating engine and moving to controller based system

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const path = require("path");
 
 const bodyParser = require("body-parser");
-
 const express = require("express");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.render("404");
+  res.render("404", { pageTitle: "Page Not Found" });
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,9 @@
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
-const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shops" });
-});
+const productsController = require("../controllers/products");
+
+router.get("/", productsController.getProducts);
 
 module.exports = router;
