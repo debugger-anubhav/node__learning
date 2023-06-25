19cedbda1ef07bbaaead476f3c73e41aca29f6de
Anukr98 <anubhav.kumar98@gmail.com>
2023-06-26 01:00:10 +0530

    added product detail page to the flow

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,25 @@ exports.getProducts = (req, res) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  console.log(prodId);
+  Product.findByID(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+  // Product.fetchAll((products) => {
+  //   res.render("shop/product-list", {
+  //     prods: products,
+  //     pageTitle: "All Products",
+  //     path: "/products",
+  //   });
+  // });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
@@ -24,6 +43,13 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
     pageTitle: "Your cart",
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your orders",
   });
 };
 
